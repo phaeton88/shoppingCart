5d4222bb9d556819b702820b3bb8024b00a7bccb
alexkostritsa <alexanderkostritsa@gmail.com>
2020-10-06 22:49:15 -0400

    Added event handlers for calc subtotal and total Added add new item function

--- js/application.js
+++ js/application.js
@@ -1,17 +1,45 @@
 $(document).ready(function () {
-  var arr = [];
-  $('tbody tr').each(function (i, el) {
-    var price = parseFloat($(el).children('.price').text());
-    var quantity = parseFloat($(el).find('.quantity input').val());
-    var subtotal = price * quantity;
-    $(el).children('.subtotal').html(subtotal);
-    arr.push(subtotal);
-  });
-  var total = arr.reduce(function (sum, num) {
-    return sum + num;
-  });
-  $('.total').html(total);
 
+
+  var calculateSubtotalTotal = function () {
+    var arr = [];
+    $('tbody tr').each(function (i, el) {
+      var price = parseFloat($(el).children('.price').text());
+      var quantity = parseFloat($(el).find('.quantity input').val());
+      var subtotal = price * quantity;
+      $(el).children('.subtotal').html(subtotal);
+      arr.push(subtotal);
+    });
+    var total = arr.reduce(function (sum, num) {
+      return sum + num;
+    });
+    $('.total').html(total);
+  };
+
+  calculateSubtotalTotal();
+
+  $('.btn.remove').on('click', function (event) {
+      $(this).closest('tr').remove();
+      calculateSubtotalTotal();
+    });
+
+  $('tr input').on('input', function () {
+    calculateSubtotalTotal();
+  });
+
+  $('#addItem').on('submit', function (event) {
+    event.preventDefault();
+    var name = $(this).children('.name').val();
+    var price = $(this).children('.price').val();
+    var quantity = $(this).children('.quantity').val();
+    $('tbody').append('<tr>' + '<td class = "name">' + name + '</td>' +
+    '<td class = "price">' + price + '</td>' +
+    '<td class="quantity"><input type="number" value="' + quantity + '" /></td>' +
+    '<td class = "subtotal">' +
+    '<td><button class="btn btn-light btn-sm remove">remove</button></td>' +
+    '</tr>');
+    calculateSubtotalTotal();
+  });
 
 
 
